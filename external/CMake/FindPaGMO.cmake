1f095b9c8dcb0e95241c280182c6a9b37ee7f523
Francesco Lupi <f.lupi@student.tudelft.nl>
2017-07-13 12:14:03 +0200

    Added pagmo2 to tudatBundle

--- external/CMake/FindPaGMO.cmake
+++ external/CMake/FindPaGMO.cmake
@@ -13,22 +13,22 @@
  #    Notes
  #	    Sets the following variables:
  #          PAGMO_INCLUDE_DIR    - Source directory to include
- #          PAGMO_LIBRARYDIR     - Path to build static library
- #          PAGMO_VERSION_OK     - True of PaGMO found.
- #	     
- #   	    PAGMO_VERSION        - PaGMO version found e.g. 1.1.5
- #   	    PAGMO_VERSION_MAJOR  - PaGMO major version found e.g. 1
- #   	    PAGMO_VERSION_MINOR  - PaGMO minor version found e.g. 1
- #   	    PAGMO_VERSION_PATCH  - PaGMO patch version found e.g. 5
+ #          	     
+ #   	    PAGMO_VERSION        - PaGMO version found e.g. 2.4.0
+ #   	    PAGMO_VERSION_MAJOR  - PaGMO major version found e.g. 2
+ #   	    PAGMO_VERSION_MINOR  - PaGMO minor version found e.g. 4
+ #   	    (PAGMO_VERSION_PATCH  - PaGMO patch version found e.g. 0)
 
 macro(_pagmo_check_version)
-  message(STATUS "Checking for PaGMO in: " ${PAGMO_BASE_PATH})      
-  file(READ "${PAGMO_BASE_PATH}/main.cpp" _pagmo_header)
-  STRING(REGEX REPLACE "^.*PaGMO ([0-9]+)\\.[0-9]+\\.[0-9]+.*" "\\1" PAGMO_VERSION_MAJOR "${_pagmo_header}")
-  STRING(REGEX REPLACE "^.*PaGMO [0-9]+\\.([0-9]+)\\.[0-9]+.*" "\\1" PAGMO_VERSION_MINOR "${_pagmo_header}")
-  STRING(REGEX REPLACE "^.*PaGMO [0-9]+\\.[0-9]+\\.([0-9]+).*" "\\1" PAGMO_VERSION_PATCH "${_pagmo_header}")
+  
+  message(STATUS "Checking for PaGMO in: ${PAGMO_BASE_PATH} and ${PAGMO_BUILD_PATH}" )
 
-  set(PAGMO_VERSION ${PAGMO_VERSION_MAJOR}.${PAGMO_VERSION_MINOR}.${PAGMO_VERSION_PATCH})
+  # Reads the version from a file in the build folder
+  file(READ "${PAGMO_BUILD_PATH}/include/pagmo/config.hpp" _pagmo_header)
+
+  STRING(REGEX REPLACE "^.*define PAGMO_VERSION_MAJOR ([0-9]+).*" "\\1" PAGMO_VERSION_MAJOR "${_pagmo_header}")
+  STRING(REGEX REPLACE "^.*define PAGMO_VERSION_MINOR ([0-9]+).*" "\\1" PAGMO_VERSION_MINOR "${_pagmo_header}")
+  set(PAGMO_VERSION ${PAGMO_VERSION_MAJOR}.${PAGMO_VERSION_MINOR}.0)
 
   # Only check version if a required is set.
   if(PaGMO_FIND_VERSION)
@@ -46,34 +46,67 @@ macro(_pagmo_check_version)
                    "but at least version ${PaGMO_FIND_VERSION} is required!")
   endif(NOT PAGMO_VERSION_OK)
 
-  set(PAGMO_LIBRARY "pagmo")
-  set(PAGMO_INCLUDE_DIR ${PAGMO_BASE_PATH}/../)
-  set(PAGMO_LIBRARYDIR ${PAGMO_BASE_PATH}/build/src/)
-  link_directories(${PAGMO_LIBRARYDIR})
+  #Set include directories
+  set(PAGMO_INCLUDE_DIR "${PAGMO_BASE_PATH}/include/;${PAGMO_BUILD_PATH}/include/")
 endmacro(_pagmo_check_version)
+
 
 if(PAGMO_BASE_PATH)
 
-  _pagmo_check_version()
-  set(PAGMO_FOUND ${PAGMO_VERSION_OK})
+  set(PAGMO_BASE_PATH_INIT ON)
 
 else (PAGMO_BASE_PATH)
-  find_path(PAGMO_BASE_PATH NAMES main.cpp
+  find_path(PAGMO_BASE_PATH NAMES pagmo-config.cmake.in
       PATHS
-      ${PROJECT_SOURCE_DIR}/../pagmo
-      ${PROJECT_SOURCE_DIR}/../../pagmo
-      ${PROJECT_SOURCE_DIR}/../../../pagmo
-      ${PROJECT_SOURCE_DIR}/../../../../pagmo
-      ${PROJECT_SOURCE_DIR}/../../../../../pagmo
+      ${PROJECT_SOURCE_DIR}/pagmo2
+      ${PROJECT_SOURCE_DIR}/../pagmo2
+      ${PROJECT_SOURCE_DIR}/../../../pagmo2
+      ${PROJECT_SOURCE_DIR}/../../../../pagmo2
+      ${PROJECT_SOURCE_DIR}/../../../../../pagmo2
     )
 
-  if(PAGMO_BASE_PATH)
-    _pagmo_check_version()
-  endif(PAGMO_BASE_PATH)
+    if(PAGMO_BASE_PATH)
+	set(PAGMO_BASE_PATH_INIT ON)
+	else(PAGMO_BUILD_PATH)
+	message(STATUS "Couldn't find PaGMO directory!")
+    endif()
 
-  include(FindPackageHandleStandardArgs)
-  find_package_handle_standard_args(PaGMO DEFAULT_MSG PAGMO_BASE_PATH PAGMO_VERSION_OK)
-
-  mark_as_advanced(PAGMO_BASE_PATH)
+    mark_as_advanced(PAGMO_BASE_PATH)
 
 endif(PAGMO_BASE_PATH)
+
+
+if(PAGMO_BUILD_PATH)
+
+  set(PAGMO_BUILD_PATH_INIT ON)
+
+else (PAGMO_BUILD_PATH)
+  find_path(PAGMO_BUILD_PATH NAMES "include/pagmo/config.hpp"
+      PATHS
+      ${CMAKE_BINARY_DIR}/pagmo2
+      ${CMAKE_BINARY_DIR}/../pagmo2
+      ${CMAKE_BINARY_DIR}/../../../pagmo2
+      ${CMAKE_BINARY_DIR}/../../../../pagmo2
+      ${CMAKE_BINARY_DIR}/../../../../../pagmo2
+    )
+
+    if(PAGMO_BUILD_PATH)
+	set(PAGMO_BUILD_PATH_INIT ON)
+    else(PAGMO_BUILD_PATH)
+	message(STATUS "Couldn't find PaGMO build directory!")
+    endif()
+
+    mark_as_advanced(PAGMO_BUILD_PATH)
+  
+endif(PAGMO_BUILD_PATH)
+
+
+if(PAGMO_BASE_PATH_INIT AND PAGMO_BUILD_PATH_INIT)
+    _pagmo_check_version()
+    set(FOUND_PAGMO ${PAGMO_VERSION_OK})
+endif()
+
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(PaGMO DEFAULT_MSG PAGMO_INCLUDE_DIR  PAGMO_VERSION_OK)
+
