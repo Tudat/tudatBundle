1a1bdd33cfbf0d4780e037cbf9e87997e643d279
Dominic Dirkx <d.dirkx@tudelft.nl>
2018-10-26 15:22:49 +0200

    Update to work with 32 and 64 bit Mingw (?)

--- external/CMake/compiler.cmake
+++ external/CMake/compiler.cmake
@@ -103,11 +103,11 @@ elseif( TUDAT_BUILD_GNU )
         set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -static-libgcc -static-libstdc++")
     endif()
 
-    if( MINGW )
+    if( MINGW AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS 5.0 )
       # MinGW gives some c11plus.xe out of memory messages:
       # http://sourceforge.net/p/mingw-w64/mailman/message/33182613/
-#      set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -ftrack-macro-expansion=0")
-#      set(CMAKE_EXE_LINKER_FLAGS "-Wl,--large-address-aware")
+      set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -ftrack-macro-expansion=0")
+      set(CMAKE_EXE_LINKER_FLAGS "-Wl,--large-address-aware")
     endif()
 
 elseif( TUDAT_BUILD_MSVC )
